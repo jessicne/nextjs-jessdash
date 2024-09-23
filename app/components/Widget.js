bcb3a811343112c71fd40f65843bb3721a2d1dda
Jessica Newport <jnewport@dollargeneral.com>
2024-09-23 08:39:52 -0500

    adding pages 	new file:   .eslintrc.json 	new file:   .gitignore 	new file:   app/components/Widget.js 	new file:   app/dashboard/customers/page.tsx 	new file:   app/dashboard/invoices/page.tsx 	new file:   app/dashboard/layout.tsx 	new file:   app/dashboard/page.tsx 	new file:   app/favicon.ico 	new file:   app/fonts/GeistMonoVF.woff 	new file:   app/fonts/GeistVF.woff 	new file:   app/globals.css 	new file:   app/layout.tsx 	new file:   app/lib/data.ts 	new file:   app/lib/definitions.ts 	new file:   app/lib/placeholder-data.ts 	new file:   app/lib/utils.ts 	new file:   app/page.tsx 	new file:   app/seed/route.ts 	new file:   app/ui/acme-logo.tsx 	new file:   app/ui/button.tsx 	new file:   app/ui/customers/table.tsx 	new file:   app/ui/dashboard/cards.tsx 	new file:   app/ui/dashboard/latest-invoices.tsx 	new file:   app/ui/dashboard/nav-links.tsx 	new file:   app/ui/dashboard/revenue-chart.tsx 	new file:   app/ui/dashboard/sidenav.tsx 	new file:   app/ui/global.css 	new file:   app/ui/invoices/breadcrumbs.tsx 	new file:   app/ui/invoices/buttons.tsx 	new file:   app/ui/invoices/create-form.tsx 	new file:   app/ui/invoices/edit-form.tsx 	new file:   app/ui/invoices/pagination.tsx 	new file:   app/ui/invoices/status.tsx 	new file:   app/ui/invoices/table.tsx 	new file:   app/ui/login-form.tsx 	new file:   app/ui/search.tsx 	new file:   app/ui/skeletons.tsx 	new file:   next.config.mjs 	new file:   package-lock.json 	new file:   package.json 	new file:   postcss.config.mjs 	new file:   public/customers/amy-burns.png 	new file:   public/customers/balazs-orban.png 	new file:   public/customers/delba-de-oliveira.png 	new file:   public/customers/evil-rabbit.png 	new file:   public/customers/lee-robinson.png 	new file:   public/customers/michael-novotny.png 	new file:   public/favicon.ico 	new file:   public/hero-desktop.png 	new file:   public/hero-mobile.png 	new file:   public/opengraph-image.png 	new file:   tailwind.config.ts 	new file:   tsconfig.json

---
+++ app/components/Widget.js
@@ -0,0 +1,24 @@
+import {useState} from 'react';
+import Head from 'next/head';
+function Widget({pageName}) {
+    const [active, setActive] = useState(false);
+    if (active) {
+        return (
+            <>
+                <Head>
+                    <title> You're browsing the {pageName} page</title>
+                </Head>
+                <div>
+                    <button onClick={() => setActive(false)}>Restore original title</button>
+                    Take a look at the title!
+                </div>
+            </>
+        );
+    }
+    return (
+        <>
+            <button onClick={() => setActive(true)}>Change title</button>
+        </>
+    );
+}
+export default Widget;
