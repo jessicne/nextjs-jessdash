6f090c75612dddd42904105fdd34794c30f45200
Jessica Newport <jnewport@dollargeneral.com>
2024-09-23 14:29:42 -0500

    comments in latestinvoice

--- app/ui/dashboard/latest-invoices.tsx
+++ app/ui/dashboard/latest-invoices.tsx
@@ -1,4 +1,4 @@
-import { ArrowPathIcon } from '@heroicons/react/24/outline';
+/*import { ArrowPathIcon } from '@heroicons/react/24/outline';*/
 /*import clsx from 'clsx';
 import Image from 'next/image';
 import { lusitana } from '@/app/ui/fonts';
@@ -10,7 +10,7 @@ export default async function LatestInvoices({
 }) {
   return (
     <div className="flex w-full flex-col md:col-span-4">
-      <h2 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+      <h2 className="mb-4 text-xl md:text-2xl">
         Latest Invoices
       </h2>
       <div className="flex grow flex-col justify-between rounded-xl bg-gray-50 p-4">
@@ -55,7 +55,7 @@ export default async function LatestInvoices({
           })}
         </div> */}
         <div className="flex items-center pb-2 pt-6">
-          <ArrowPathIcon className="h-5 w-5 text-gray-500" />
+          {/*<ArrowPathIcon className="h-5 w-5 text-gray-500" />*/}
           <h3 className="ml-2 text-sm text-gray-500 ">Updated just now</h3>
         </div>
       </div>
